65e6d1a21721e8a9fa1aa2e4e72b05663ecdf734
Sahil Prasad <sahilprasad312@gmail.com>
2026-02-07 08:22:32 +0000

    main.js

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ const loginForm =
   document.queryselector(".form.login");
 const registerHero = 
   document.queryselector(".hero.register");
+const registerForm = 
+  document.queryselector(".form.register");
+const cardBg = 
+  document.queryselector(".card-bg");
+  
+const toggleView = () => {
+  const loginActive =
+  loginHero.classList.contains("active");
+  
+  cardBg.classList.togglr("login", !loginActive);
+
+  [loginHero , loginForm]
+    .forEach(el =>
+      el
+    .classList
+    .toggle("active"));
+  [registerHero, registerForm]
+    .forEach(el =>
+    el
+    .classList
+    .toggle("active"));
+}  
