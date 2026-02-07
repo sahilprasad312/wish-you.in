c0b8e6d88cb1b46bc8d9eff769ac35ac8b6f2fa4
Sahil Prasad <sahilprasad312@gmail.com>
2026-02-07 13:34:09 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const loginHero =
+  document.queryselector(".hero.login");
+const loginForm = 
+  document.queryselector(".form.login");
+const registerHero = 
+  document.queryselector(".hero.register");
